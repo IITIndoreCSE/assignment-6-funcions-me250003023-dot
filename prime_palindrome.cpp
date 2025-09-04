c2f9e1ac1ab038170993d37385c54eca8548fab6
me250003023-dot <me250003023@iiti.ac.in>
2025-09-04 12:25:59 +0530

    Update prime_palindrome.cpp

--- prime_palindrome.cpp
+++ prime_palindrome.cpp
@@ -2,49 +2,44 @@
 using namespace std;
 
 bool isPrime(int n) {
-    // TODO: complete the function as per instructions
-    if(n<2){
+    if (n < 2)
         return false;
-    }
-    if(n==2){
+
+    if (n == 2)
         return true;
-    }
-    if(n%2==0){
+
+    if (n % 2 == 0)
         return false;
-    }
-    for(int i=3;i*i<=n;i+=2){
-        if(n%i==0){
+
+    for (int i = 3; i * i <= n; i += 2)
+        if (n % i == 0)
             return false;
-        }
-    }
+
     return true;
 }
 
 bool isPalindrome(int n) {
-    // TODO: complete the function as per instructions
     int org = n;
     int rev = 0;
 
-    
     while (n > 0) {
-        int digit = n % 10; 
-        rev = rev * 10 + digit; 
-        n /= 10;  
+        int digit = n % 10;
+        rev = rev * 10 + digit;
+        n /= 10;
     }
-        
 
-    return org==rev;
-
+    return org == rev;
 }
 
 bool primePalindrome(int n) {
-    // TODO: complete the function as per instructions
-    return isPrime && isPalindrome(n);
-    
+    return isPrime(n) && isPalindrome(n);
 }
 
 int main() {
-    int n; cin >> n;
+    int n;
+    cin >> n;
+
     cout << (primePalindrome(n) ? 1 : 0) << "\n";
+
     return 0;
 }
